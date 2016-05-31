19b8a4c65b8aa8fd6edc25b8099b4ee4d7bb63c3
chrisseg <chrisseg@DOPPLER>
2016-05-30 20:48:06 -0700

    Working Azure PS script

---
+++ Azure_AzureStack_Automation/Azure_AzureStack_Deploy.ps1
@@ -0,0 +1,34 @@
+﻿# Run this command to add your Azure/Azure Stack account if you have not already done so
+Add-AzureRmAccount
+
+# Gets the subscription ID that we want to use and sets it as the current context.
+Get-AzureRmSubscription -SubscriptionName "Visual Studio Enterprise" | Set-AzureRmContext
+
+# Set deployment location and resource group variables
+$locName = "West US" # Azure (change to the region of your choice)
+# $locName = "local" # Azure Stack
+$rgName = "iottest"
+$deployName = "iottestdeploy"
+$scriptRoot = "C:\INSERT ROOT HERE"
+$templatePath = "$scriptRoot\azuredeploy.json"
+
+# Create Resource Group
+New-AzureRmResourceGroup -Name $rgName -Location $locName
+
+# Deployment parameters
+$deployParam = @{
+	"adminUsername" = "localAdmin";
+	"adminPassword" = "P@ssw0rd1";
+	"deploymentEnv" = "Azure"; # Deploy to Azure
+	# "deploymentEnv" = "AzureStack"; # Change for Azure Stack
+
+}
+
+# Deploy Template
+New-AzureRmResourceGroupDeployment `
+	-Name $deployName `
+	-ResourceGroupName $rgName `
+	-Mode Complete `
+	-Force `
+	-TemplateFile $templatePath `
+	-TemplateParameterObject $deployParam
